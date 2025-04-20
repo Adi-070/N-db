b314d42a93fcad179c6ac31ca9401853643e9f23
Aditya Mishra <mishraaditya760@gmail.com>
2025-04-20 12:37:59 +0530

    config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true, // or false, depending on whether this is a permanent redirect
+      },
+    ];
+  },
 };
 
 export default nextConfig;
